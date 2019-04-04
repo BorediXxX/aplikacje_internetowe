398c7c6465b9266275e21039633eede733c89d6c
Boredix <boredixxx@gmail.com>
2019-04-04 11:47:35 +0200

    n

--- Lab2/calc.js
+++ Lab2/calc.js
@@ -1,10 +1,35 @@
-const double = x => 2 * x;
-function init(e,x) {
-console.error(typeof e);
-console.info(double(x));
-console.log('Container:',document.getElementById('container'));
-   
+const pola = [
+    { txt: 0, row:5, col:'1/3'},
+    { txt: 1, row:4, col:1},
+    { txt: 2, row:4, col:2},
+    { txt: 3, row:4, col:3},
+    { txt: 4, row:3, col:1},
+    { txt: 5, row:3, col:2},
+    { txt: 6, row:3, col:3},
+    { txt: 7, row:2, col:1},
+    { txt: 8, row:2, col:2},
+    { txt: 9, row:2, col:3},
+    { txt:'C', row:2, col:4},
+    { txt: '+', row:3, col:4},
+    { txt: '-', row:4, col:4},
+    { txt: '=', row:5, col:4},
+    { txt: '.', row:5, col:3},
+    { txt: 'Display', row:1, col:'1/5'},
+    ];
+const init = () => {
+    const container = document.createElement('div');
+    container.id = 'container';
+    
+    pola.forEach(el => {
+        const div = document.createElement('div');
+        div.textContent = el.txt;
+        div.style.gridColumn = el.col;
+        div.style.gridRow = el.row;
+        container.appendChild(div);
+     
+    });
+    document.body.appendChild(container); 
+}
 
 
-window.addEventListener('DOMContentLoaded', e => init(e,5));
-
+window.addEventListener('DOMContentLoaded', init);
